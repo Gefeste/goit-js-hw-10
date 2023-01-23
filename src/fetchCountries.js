a32ca4596b9ef64ebe45d4f6b11d8f8bbef56bd5
Gefeste <gefeste111@gmail.com>
2023-01-23 14:52:32 +0100

    on-finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,12 @@
+import Notiflix from 'notiflix';
+
 function fetchCountries(name) {
     const BASE_URL = `https://restcountries.com/v3.1/name/`
 
     return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`).then(resp => {
         // console.log(resp)
         if (!resp.ok) {
-        //    throw new Error(resp.statusText)
+        Notiflix.Notify.failure('Oops, there is no country with that name');
         }
         return resp.json()
    })
--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import { fetchCountries } from "./fetchCountries.js";
 import './css/styles.css';
 import debounce from "lodash.debounce";
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const list = document.querySelector('.country-list')
 const input = document.querySelector('#search-box');
+const remo = document.querySelector('.js-remove')
 input.addEventListener('input', debounce(onLoad, DEBOUNCE_DELAY));
 console.log()
 
 
 
 function onLoad(eve) {
-    fetchCountries(eve.target.value)
+    if (eve.target.value.trim() === "") {
+        list.innerHTML = "";
+        return
+    };
+    
+    fetchCountries(eve.target.value.trim())
         .then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+                return
+            };
             if (data.length > 1) {
         list.innerHTML = createMarkup(data)
     } else {
         list.innerHTML = createMarkupCountry(data)
-    }
+            };
         })
 };
 
@@ -34,12 +45,15 @@ function createMarkupCountry(arr) {
         capital,
         population,
         languages
-        }) =>
-        `<img src="${svg}" width="50" alt="flag">
+    }) =>
+        
+        `<div class="js-remove">
+        <img src="${svg}" width="50" alt="flag">
         <h1>${official}</h1>
         <p>Capital: ${capital}</p>
         <p>Population: ${population}</p>
-        <p>Languages: ${Object.values(languages)}</p>`
+                <p>Languages: ${Object.values(languages)}</p>
+        </div>`
     ).join('')
 }
 
@@ -53,12 +67,14 @@ function createMarkup(arr) {
                 svg
             }
         }) =>
-            `<li class=""js-list>
+            `<div class="js-remove">
+            <li class=""js-list>
         <div class="js-box">
         <img src="${svg}" width="30" alt="flag">
         <p>${official}</p>
         </div>
-        </li>`
+        </li>
+        </div>`
         ).join('')
     }
 
